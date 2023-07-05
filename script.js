6ddba46c2becea25f4b7bd7b7fd9ef7410631097
KKaged <dhigaredaleon707@gmail.com>
2023-07-05 21:42:44 +0900

    can now add multiple cards.

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ var title = document.querySelector("#title");
 var author = document.querySelector("#author");
 var pages = document.querySelector("#pages");
 var submit = document.querySelector(".form-submit");
-var newCard = document.createElement("div");
-var titleDIV = document.createElement("div");
-var authorDIV = document.createElement("div");
-var pagesDIV = document.createElement("div");
 
 document.querySelector("#show-form").addEventListener("click", function () {
   document.querySelector(".pop-up").classList.add("active");
 });
 
 submit.addEventListener("click", function () {
+  var newCard = document.createElement("div"); // Create a new instance of newCard each time
+  var titleDIV = document.createElement("div");
+  var authorDIV = document.createElement("div");
+  var pagesDIV = document.createElement("div");
   newCard.classList.add("book-container");
   titleDIV.classList.add("book-title");
   authorDIV.classList.add("book-author");
