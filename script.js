e0d6e1e8a0cbbfcfd86a3b11fe161fb932b0ae3c
KKaged <dhigaredaleon707@gmail.com>
2023-07-05 18:29:09 +0900

    added pop-up and JS so that it works.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function popup() {
-  console.log("pressed!");
-}
+var title = document.querySelector("#title").value;
+var author = document.querySelector("#author").value;
+var pages = document.querySelector("#pages").value;
+const submit = document.querySelector(".form-submit");
+
+document.querySelector(`#show-form`).addEventListener("click", function () {
+  document.querySelector(".pop-up").classList.add("active");
+});
+
+submit.addEventListener("click", function () {
+  document.querySelector(".pop-up").classList.remove("active");
+});
